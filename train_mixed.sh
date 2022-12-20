ddb050c2bdf682ec9515562427354470794565fc
NOBEL DANG <nobeldang@gmail.com>
2022-12-20 17:39:15 -0500

    adding files and results

---
+++ train_mixed.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p checkpoints
+python3 -u train.py --name raft-sintel --stage sintel --validation sintel --gpus 0 --num_steps 25000 --batch_size 5 --lr 0.0001 --image_size 368 768 --wdecay 0.00001 --gamma=0.85 --mixed_precision
